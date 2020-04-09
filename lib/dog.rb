4bae50234bb30cb1a52292ebc16a46d9d13835e3
Samantha Jordan <shinyguns@gmail.com>
2020-04-09 19:58:30 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -5,5 +5,13 @@ class Dog
   :database => "db/students.sqlite"
 )
 
-
+sql = <<-SQL
+  CREATE TABLE IF NOT EXISTS students (
+  id INTEGER PRIMARY KEY,
+  name TEXT
+  )
+SQL
+ 
+# Remember, the previous step has to run first so that `connection` is set!
+ActiveRecord::Base.connection.execute(sql)
 end
